f9a5b868c3eba819d6f0e54d0ddf8be3313e9a4a
Julian Scipioni <167777190+JulianScipioni06@users.noreply.github.com>
2025-10-20 09:15:06 -0300

    commit

--- TP2/Ejercicio1/consultas.sql
+++ TP2/Ejercicio1/consultas.sql
@@ -1,20 +1,28 @@
 -- CONSULTAS SIN JOIN
 USE ejercicio1_tp2;
 -- 1. ¿Qué socios tienen barcos amarrados en un número de amarre mayor que 10?
-SELECT s.id_socio, s.nombre, b.numero_amarre FROM Socios as s, Barcos as b WHERE s.id_socio = b.id_socio AND numero_amarre > 10;
-
+SELECT * FROM Socios WHERE id_socio IN (
+  SELECT id_socio FROM Barcos WHERE numero_amarre > 10
+);
 -- 2. ¿Cuáles son los nombres de los barcos y sus cuotas de aquellos barcos cuyo socio se llama 'Juan Pérez'?
-SELECT socio.nombre, barco.nombre AS Nombre_Barco, barco.cuota FROM Barcos AS barco, Socios AS socio WHERE barco.id_socio = socio.id_socio AND socio.nombre = 'Juan Pérez';
+SELECT nombre, cuota FROM Barcos IN (
+  SELECT id_socio FROM Socios WHERE nombre = 'Juan Pérez';
+)
 
 -- 3. ¿Cuántas salidas ha realizado el barco con matrícula 'ABC123'?
-SELECT matricula, COUNT(*) AS Total_Salidas FROM Salidas WHERE matricula IN (SELECT matricula FROM Barcos WHERE matricula = 'ABC123');
+SELECT COUNT(*) FROM Salidas WHERE matricula IN (
+  SELECT matricula from Barcos WHERE matricula = 'ABC123'
+);
 
 -- 4. Lista los barcos que tienen una cuota mayor a 500 y sus respectivos socios.
-SELECT s.nombre, b.nombre, b.numero_amarre, b.cuota FROM Barcos AS b, Socios AS s WHERE b.id_socio = s.id_socio AND b.cuota > 500;
+SELECT * FROM Barcos WHERE cuota > 500 AND id_socio IN(
+  SELECT id_socio FROM Socios
+);
 
 -- 5. ¿Qué barcos han salido con destino a 'Mallorca'?
-SELECT b.matricula, b.nombre, s.destino FROM Barcos AS b, Salidas AS s WHERE b.matricula = s.matricula AND s.destino = 'Mallorca';
-
+SELECT * FROM Barcos WHERE matricula IN (
+	SELECT matricula FROM Salidas WHERE destino = 'Mallorca'
+);
 -- 6. ¿Qué patrones (nombre y dirección) han llevado un barco cuyo socio vive en 'Barcelona'?
 SELECT patron_nombre, patron_direccion FROM Salidas WHERE matricula IN 
 (SELECT matricula FROM Barcos
